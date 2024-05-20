43196fa66bb3fec9d2e7046dd76c2a8b422a9d82
Paolo Galli <paolo.galli@vechain.org>
2024-05-20 16:39:54 +0200

    refactor: remove not supported tests

--- test/utils/cryptography/ECDSA.test.js
+++ test/utils/cryptography/ECDSA.test.js
@@ -1,5 +1,4 @@
-require('@openzeppelin/test-helpers');
-const { expectRevertCustomError } = require('../../helpers/customError');
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const { toEthSignedMessageHash } = require('../../helpers/sign');
 
 const { expect } = require('chai');
@@ -51,50 +50,27 @@ contract('ECDSA', function (accounts) {
 
   context('recover with invalid signature', function () {
     it('with short signature', async function () {
-      await expectRevertCustomError(this.ecdsa.$recover(TEST_MESSAGE, '0x1234'), 'ECDSAInvalidSignatureLength', [2]);
+      await expectRevert.unspecified(this.ecdsa.$recover(TEST_MESSAGE, '0x1234'));
     });
 
     it('with long signature', async function () {
-      await expectRevertCustomError(
+      await expectRevert.unspecified(
         // eslint-disable-next-line max-len
         this.ecdsa.$recover(
           TEST_MESSAGE,
           '0x01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
-        ),
-        'ECDSAInvalidSignatureLength',
-        [85],
+        )
       );
     });
   });
 
   context('recover with valid signature', function () {
     context('using web3.eth.sign', function () {
-      it('returns signer address with correct signature', async function () {
-        // Create the signature
-        const signature = await web3.eth.sign(TEST_MESSAGE, other);
-
-        // Recover the signer address from the generated message and signature.
-        expect(await this.ecdsa.$recover(toEthSignedMessageHash(TEST_MESSAGE), signature)).to.equal(other);
-      });
-
-      it('returns signer address with correct signature for arbitrary length message', async function () {
-        // Create the signature
-        const signature = await web3.eth.sign(NON_HASH_MESSAGE, other);
-
-        // Recover the signer address from the generated message and signature.
-        expect(await this.ecdsa.$recover(toEthSignedMessageHash(NON_HASH_MESSAGE), signature)).to.equal(other);
-      });
-
-      it('returns a different address', async function () {
-        const signature = await web3.eth.sign(TEST_MESSAGE, other);
-        expect(await this.ecdsa.$recover(WRONG_MESSAGE, signature)).to.not.equal(other);
-      });
-
       it('reverts with invalid signature', async function () {
         // eslint-disable-next-line max-len
         const signature =
           '0x332ce75a821c982f9127538858900d87d3ec1f9f737338ad67cad133fa48feff48e6fa0c18abc62e42820f05943e47af3e9fbe306ce74d64094bdf1691ee53e01c';
-        await expectRevertCustomError(this.ecdsa.$recover(TEST_MESSAGE, signature), 'ECDSAInvalidSignature', []);
+        await expectRevert.unspecified(this.ecdsa.$recover(TEST_MESSAGE, signature));
       });
     });
 
@@ -142,12 +118,10 @@ contract('ECDSA', function (accounts) {
       it('reverts wrong v values', async function () {
         for (const v of ['00', '01']) {
           const signature = signatureWithoutV + v;
-          await expectRevertCustomError(this.ecdsa.$recover(TEST_MESSAGE, signature), 'ECDSAInvalidSignature', []);
+          await expectRevert.unspecified(this.ecdsa.$recover(TEST_MESSAGE, signature));
 
-          await expectRevertCustomError(
-            this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, ...split(signature)),
-            'ECDSAInvalidSignature',
-            [],
+          await expectRevert.unspecified(
+            this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, ...split(signature))
           );
         }
       });
@@ -155,10 +129,8 @@ contract('ECDSA', function (accounts) {
       it('rejects short EIP2098 format', async function () {
         const v = '1b'; // 27 = 1b.
         const signature = signatureWithoutV + v;
-        await expectRevertCustomError(
-          this.ecdsa.$recover(TEST_MESSAGE, to2098Format(signature)),
-          'ECDSAInvalidSignatureLength',
-          [64],
+        await expectRevert.unspecified(
+          this.ecdsa.$recover(TEST_MESSAGE, to2098Format(signature))
         );
       });
     });
@@ -206,12 +178,10 @@ contract('ECDSA', function (accounts) {
       it('reverts invalid v values', async function () {
         for (const v of ['00', '01']) {
           const signature = signatureWithoutV + v;
-          await expectRevertCustomError(this.ecdsa.$recover(TEST_MESSAGE, signature), 'ECDSAInvalidSignature', []);
+          await expectRevert.unspecified(this.ecdsa.$recover(TEST_MESSAGE, signature));
 
-          await expectRevertCustomError(
-            this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, ...split(signature)),
-            'ECDSAInvalidSignature',
-            [],
+          await expectRevert.unspecified(
+            this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, ...split(signature))
           );
         }
       });
@@ -219,10 +189,8 @@ contract('ECDSA', function (accounts) {
       it('rejects short EIP2098 format', async function () {
         const v = '1c'; // 27 = 1b.
         const signature = signatureWithoutV + v;
-        await expectRevertCustomError(
-          this.ecdsa.$recover(TEST_MESSAGE, to2098Format(signature)),
-          'ECDSAInvalidSignatureLength',
-          [64],
+        await expectRevert.unspecified(
+          this.ecdsa.$recover(TEST_MESSAGE, to2098Format(signature))
         );
       });
     });
@@ -233,11 +201,9 @@ contract('ECDSA', function (accounts) {
       const highSSignature =
         '0xe742ff452d41413616a5bf43fe15dd88294e983d3d36206c2712f39083d638bde0a0fc89be718fbc1033e1d30d78be1c68081562ed2e97af876f286f3453231d1b';
       const [r, v, s] = split(highSSignature);
-      await expectRevertCustomError(this.ecdsa.$recover(message, highSSignature), 'ECDSAInvalidSignatureS', [s]);
-      await expectRevertCustomError(
-        this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, r, v, s),
-        'ECDSAInvalidSignatureS',
-        [s],
+      await expectRevert.unspecified(this.ecdsa.$recover(message, highSSignature), 'ECDSAInvalidSignatureS', [s]);
+      await expectRevert.unspecified(
+        this.ecdsa.methods['$recover(bytes32,uint8,bytes32,bytes32)'](TEST_MESSAGE, r, v, s)
       );
       expect(() => to2098Format(highSSignature)).to.throw("invalid signature 's' value");
     });
