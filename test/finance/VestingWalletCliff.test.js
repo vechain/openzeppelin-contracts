556b2ed3d3a5a2c9eed94264d8f34d69eb024a4b
Mog.Lu <mog.lu2017@outlook.com>
2026-03-30 10:19:21 +0800

    Add vesting wallet with cliff and VeChain test support

--- test/finance/VestingWalletCliff.test.js
+++ test/finance/VestingWalletCliff.test.js
@@ -1,4 +1,4 @@
-const { constants } = require('@openzeppelin/test-helpers');
+const { BN } = require('@openzeppelin/test-helpers');
 const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const { expect } = require('chai');
 const { BNmin } = require('../helpers/math');
@@ -7,24 +7,26 @@ const time = require('../helpers/time');
 
 const { envSetup, shouldBehaveLikeVesting } = require('./VestingWallet.behavior');
 
-const VestingWallet = artifacts.require('VestingWallet');
+const VestingWalletCliff = artifacts.require('$VestingWalletCliff');
 const ERC20 = artifacts.require('$ERC20');
 
-contract('VestingWallet', function (accounts) {
+contract('VestingWalletCliff', function (accounts) {
   const [sender, beneficiary] = accounts;
 
   const amount = web3.utils.toBN(web3.utils.toWei('100'));
   const duration = web3.utils.toBN(4 * 365 * 86400); // 4 years
+  const cliffDuration = web3.utils.toBN(365 * 86400); // 1 year
 
   beforeEach(async function () {
     this.start = (await time.clock.timestamp()).addn(3600); // in 1 hour
-    this.mock = await VestingWallet.new(beneficiary, this.start, duration);
+    this.cliff = this.start.add(cliffDuration);
+    this.mock = await VestingWalletCliff.new(beneficiary, this.start, duration, cliffDuration);
     this.beneficiary = beneficiary;
   });
 
-  it('rejects zero address for beneficiary', async function () {
+  it('rejects a larger cliff than vesting duration', async function () {
     await expectThorRevert(
-      VestingWallet.new(constants.ZERO_ADDRESS, this.start, duration),
+      VestingWalletCliff.new(beneficiary, this.start, duration, duration.addn(1)),
       '',
       expectRevertCheckStrategy.unspecified,
     );
@@ -35,6 +37,7 @@ contract('VestingWallet', function (accounts) {
     expect(await this.mock.start()).to.be.bignumber.equal(this.start);
     expect(await this.mock.duration()).to.be.bignumber.equal(duration);
     expect(await this.mock.end()).to.be.bignumber.equal(this.start.add(duration));
+    expect(await this.mock.cliff()).to.be.bignumber.equal(this.cliff);
   });
 
   describe('vesting schedule', function () {
@@ -42,7 +45,8 @@ contract('VestingWallet', function (accounts) {
       this.schedule = Array(64)
         .fill()
         .map((_, i) => web3.utils.toBN(i).mul(duration).divn(60).add(this.start));
-      this.vestingFn = timestamp => BNmin(amount, amount.mul(timestamp.sub(this.start)).div(duration));
+      this.vestingFn = timestamp =>
+        BNmin(amount, timestamp.lt(this.cliff) ? new BN(0) : amount.mul(timestamp.sub(this.start)).div(duration));
 
       this.token = await ERC20.new('Name', 'Symbol');
       await this.token.$_mint(this.mock.address, amount);
